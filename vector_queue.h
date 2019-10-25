bbeb56025aaef39da6d523035ddc3de86df9fd7a
Larissa Sales <larissa.s.sales@outlook.com>
2019-10-25 20:27:40 -0300

    Add vector queue library

---
+++ vector_queue.h
@@ -0,0 +1,18 @@
+#ifndef _VECTOR_QUEUE_H
+#define _VECTOR_QUEUE_H
+
+void create();
+
+int insert(int x);
+
+int remove(int *value);
+
+int is_full();
+
+int is_empty();
+
+int size();
+
+void print();
+
+#endif
