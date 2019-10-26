470995460a3b09ed437f2d299b238ad42a255368
Larissa Sales <larissa.s.sales@outlook.com>
2019-10-25 21:14:34 -0300

    Implemented library functions into circular queue linked list

---
+++ circular_queue_linked_list.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "circular_queue_linked_list.h"
+
+typedef struct cell{
+    int data;
+    struct cell *next;
+} cell;
+
+cell *queue;
+
+void create(){
+    queue = malloc(sizeof(cell));
+    queue->next = queue;
+}
+
+int insert(int x);{
+    cell *new;
+    new = malloc(sizeof(cell));
+
+    if(is_full(new)){
+        return 0;
+    }
+    new->next = queue->next;
+    queue->next = new;
+    queue->data = x
+    queue = new;
+    return 1;
+}
+
+int remove(int *value){
+    if(is_empty()){
+        return 0;
+    }
+    cell *nothing = queue->next;
+    *value = nothing->data;
+    queue->next = nothing->next;
+    free(nothing);
+    return 1;
+}
+
+int is_full(cell *c){
+    if(c!=NULL){
+        return 1;        
+    } 
+    else{
+        return 0;
+    }
+}
+
+int is_empty(){
+    if(queue->next==queue){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int size(){
+    if(queue->next == NULL){
+        return 0;
+    }
+    int sum=0;
+
+    cell *count = queue;
+
+    while(count->next != queue){
+        sum++;
+        count = count->next;
+    }
+    return sum;
+}
+
+void print();
